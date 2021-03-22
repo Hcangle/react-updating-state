00c5cc0f0737fa445b3116e1830404a1ff4265af
Hangle <hannahcangle@gmail.com>
2021-03-22 15:52:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <div>
+//     Mount Components Here
+//   </div>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ClickityClick from './components/ClickityClick';
+import LightSwitch from './components/LightSwitch.js';
+
 ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+<div>
+  <ClickityClick />
+  <LightSwitch />
+</div>,
+   document.getElementById('root')); 
